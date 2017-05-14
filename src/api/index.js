e69773e0da45bd1c82b964ebeb0013bca5f50bdd
simpleux <junaid@simpleux.co.uk>
2017-05-14 17:29:38 +0100

    Change API handling to superagent and re-testing API

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,68 @@
-/**
-* This is an example request. Create your own using best practises for
-* handling asynchronous data fetching
-**/
+import request from 'superagent';
 
-export const getData = (cb) => {
-    const vehicles = new XMLHttpRequest();
-    vehicles.open('GET', 'http://localhost:9988/api/vehicle');
+const API_SERVER = process.env && process.env.API_SERVER ? process.env.API_SERVER : document.location.protocol + "//" + document.location.host
 
-    vehicles.onreadystatechange = function() {
-        if(vehicles.readyState === 4) {
- 		    if(vehicles.status === 200) {
- 			    cb(vehicles.responseText);
-		    }
-		}
-	};
+class API {
 
-	vehicles.send();
-};
+	constructor() {
+		this.server = API_SERVER;
+	}
+
+	vehicles() {
+		return this._query('api/vehicle', null, 'vehicles');
+	}
+
+	vehicle(id) {
+		return this._query(`api/vehicle/${id}`);
+	}
+
+	setServer(server) {
+		this.server = server;
+		return this;
+	}
+
+	getServer() {
+		return this.server;
+	}
+
+	_absoluteURL(url) {
+		return `${this.server}/${url}`;
+	}
+
+	_query(api_url, params = null, responseKey = null) {
+		return new Promise((resolve, reject) => {
+
+			function processResponse(err, res) {
+
+				//@FIXME The API Server is not responding with Correct Status Code and Exposes the Raw Error Messages
+				if (err || !res.ok) {
+					return reject(err.text);
+				}
+
+
+				if(res.body && typeof res.body === 'object') {
+					if(responseKey) {
+						return resolve(res.body[responseKey]);
+					}
+				}
+
+				resolve(res.body);
+			}
+
+			if (params) {
+				return request.post(this._absoluteURL(api_url))
+					.send(params)
+					.retry(3)
+					.end(processResponse);
+			}
+
+			return request.get(this._absoluteURL(api_url))
+				.retry(3)
+				.end(processResponse);
+		});
+	}
+}
+
+const api = new API;
+
+export default api;
--- test/api.spec.js
+++ test/api.spec.js
@@ -1,13 +1,21 @@
-const expect  = require('chai').expect;
-const getData = require('~/api').getData;
+import { expect } from 'chai';
+import api        from '~/api';
 
 describe("getData example test", function () {
 
-    it('should respond with an array of vehicles', (done) => {
-        getData((response) => {
-            const data = JSON.parse(response);
-            expect(Array.isArray(data.vehicles)).to.equal(true);
+    it('should respond with an array of vehicles', async(done) => {
+
+        try {
+
+            const data = await api.vehicles();
+            expect(data).to.be.instanceof(Array);
+            expect(data).to.have.length.above(0);
+
             done();
-        })
-    })
+
+        } catch (err) {
+            done(err);
+        }
+    });
+
 });
